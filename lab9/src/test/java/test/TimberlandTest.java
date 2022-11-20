e0ecc5d9b85baa0e335995404cb7c2e907e91e73
Travkina Vlada <travkina.vlada912@gmail.com>
2022-11-20 17:12:10 +0300

    add lab 9

--- lab9/src/test/java/test/TimberlandTest.java
+++ lab9/src/test/java/test/TimberlandTest.java
@@ -15,47 +15,32 @@ import java.time.Duration;
 
 public class TimberlandTest {
     private static WebDriver driver;
+    private WebDriverWait wait;
 
     @BeforeMethod(alwaysRun = true)
     public void browserSetup() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
-//    @Test
-//    public void testAddItemToCart() {
-//        driver.get("https://www.timberland.com/shop/mens-davis-square-chukka-shoes-blue-a1sf3019");
-//        String EXPECTED_AMOUNT = "1";
-//
-//        WebElement buttonSize = new WebDriverWait(driver, Duration.ofSeconds(20))
-//                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[@data-attribute-value='7.5']")));
-//
-//        //        Thread.sleep(3000);
-//        buttonSize.click();
-//
-//        WebElement buttonAddToCart = new WebDriverWait(driver, Duration.ofSeconds(5))
-//                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[@data-add-to-cart-text='Add to Cart']")));
-//        buttonAddToCart.click();
-//
-//        WebElement amountOfProductsInCart = new WebDriverWait(driver, Duration.ofSeconds(15))
-//                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//ul[@class='topnav-utility']//span[@class='topnav-cart-qty']")));
-//
-//        //        Thread.sleep(3000);
-//        Assert.assertEquals(amountOfProductsInCart.getText(), EXPECTED_AMOUNT);
-//    }
-
     @Test
-    public void testAddItemToCartWithThreadSleep() throws InterruptedException {
+    public void testAddItemToCart() {
         driver.get("https://www.timberland.com/shop/mens-davis-square-chukka-shoes-blue-a1sf3019");
         String EXPECTED_AMOUNT = "1";
-        WebElement buttonSize = driver.findElement(By.xpath("//button[@data-attribute-value='7.5']"));
-        WebElement buttonAddToCart =  driver.findElement(By.xpath("//button[@data-add-to-cart-text='Add to Cart']"));
-        WebElement amountOfProductsInCart =   driver.findElement(By.xpath("//ul[@class='topnav-utility']//span[@class='topnav-cart-qty']"));
 
-        Thread.sleep(3000);
+        WebElement buttonSize = driver.findElement(By.xpath("//button[@data-attribute-value='7.5']"));
+        wait.until(ExpectedConditions.elementToBeClickable(buttonSize));
         buttonSize.click();
+
+        WebElement buttonAddToCart = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[@data-add-to-cart-text='Add to Cart']")));
         buttonAddToCart.click();
-        Thread.sleep(3000);
+
+        WebElement amountOfProductsInCart = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//ul[@class='topnav-utility']//span[@class='topnav-cart-qty']")));
+
+        WebElement navigatonCart = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//article[@class='topnav-minicart-content-container shopcart cart-list']")));
+        wait.until(ExpectedConditions.visibilityOf(navigatonCart));
+
         Assert.assertEquals(amountOfProductsInCart.getText(), EXPECTED_AMOUNT);
     }
 
@@ -64,16 +49,14 @@ public class TimberlandTest {
         driver.get("https://www.timberland.com/homepage.html");
         String EXPECTED_ID = "8330R236";
 
-        WebElement searchBox = new WebDriverWait(driver, Duration.ofSeconds(5))
-                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@name='searchTerm']")));
+        WebElement searchBox = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@name='searchTerm']")));
         searchBox.sendKeys("8330R236");
 
-        WebElement buttonSearch = new WebDriverWait(driver, Duration.ofSeconds(5))
-                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[@aria-label='Submit Search']")));
+        WebElement buttonSearch = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[@aria-label='Submit Search']")));
         buttonSearch.click();
 
-        WebElement idOfItem = new WebDriverWait(driver, Duration.ofSeconds(5))
-                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[@class='product-style-js']")));
+        WebElement idOfItem = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[@class='product-style-js']")));
+        wait.until(ExpectedConditions.visibilityOf(idOfItem));
 
         Assert.assertEquals(idOfItem.getText(), EXPECTED_ID);
     }
